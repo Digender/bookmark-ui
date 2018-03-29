1be7e01ee5e2cb02bcca96a6e7cf39998b15e284
Digender <mdigender@gmail.com>
2018-03-30 00:02:40 +0530

    feat: create URL added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {
+
+  }
   title = 'app';
+  public data = {};
+
+
+  createBookmark() {
+  alert('yey')
+  console.log(this.data);
+  	this
+  	  .http
+  	  .post('http://localhost:3000/api/bookmarks/', this.data, httpOptions)
+  	  .subscribe(
+       data => {
+         // refresh the list
+      	 console.log(data);
+         return true;
+       },
+       error => {
+         console.error("Error saving food! " + err);
+         return Observable.throw(error);
+       }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -13,6 +15,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule,
     NgbModule.forRoot()
   ],
